3deb8946721ab184bf443a77043fecee97e870c3
D-yer <1140440307@qq.com>
2022-10-09 14:46:35 +0800

    Integrated interactive features

--- main.cpp
+++ main.cpp
@@ -51,14 +51,8 @@ void test4() {
 // 测试Processing类（几何变化部分）
 void test5() {
 	string location = "data/", file = "test.bmp";
-
-	//PrintHintMessageB("请输入图片所在文件夹地址（在代码同一目录data下可直接输入data/）");
-	//cin >> location;
-	//PrintHintMessageA();
-
-	//PrintHintMessageB("请输入需要处理的图片文件名");
-	//cin >> file;
-	//PrintHintMessageA();
+	//InputMessage("请输入图片所在文件夹地址（在代码同一目录data下可直接输入data/）", location);
+	//InputMessage("请输入需要处理的图片文件名", file);
 
 	Img img(file, location);
 	Processing tools;
@@ -66,15 +60,49 @@ void test5() {
 	// Mirror
 	// tools.DisplayEffect(img, tools.GeometricTransform.Mirror(img));
 
-	// Scaling
-	//PrintHintMessageB("请输入放缩倍数（等比例放缩）");
-	//float multiple;
-	//cin >> multiple;
-	//PrintHintMessageA();
-	//tools.DisplayEffect(img, tools.GeometricTransform.Scaling(img, multiple));
-
 	// Rotation
-	tools.DisplayEffect(img, tools.GeometricTransform.Rotation(img, -30, 10, 100));
+	//tools.DisplayEffect(img, tools.GeometricTransform.Rotation(img, -30, 10, 100));
+
+	// Scaling
+	float multiple;
+	InputMessage("请输入放缩倍数（等比例放缩）", multiple);
+	tools.DisplayEffect(img, tools.GeometricTransform.Scaling(img, multiple));
+}
+
+
+void test6() {
+	string location, file;
+	float angle, multiple;
+	int rx, ry, mode, state;
+
+	InputMessage("请输入图片所在文件夹地址（在代码同一目录data下可直接输入data/）", location);
+	InputMessage("请输入需要处理的图片文件名", file);
+
+	Img img(file, location);
+	Processing tools;
+
+	while (true) {
+		InputMessage("请选择想要进行的操作（1：Mirror，2：Rotation，3：Scaling）", mode);
+		switch (mode) {
+		case 1: 
+			tools.DisplayEffect(img, tools.GeometricTransform.Mirror(img));
+			break;
+		case 2:
+			InputMessage("请输入旋转角度（顺时针为正值）和旋转中心（x, y），其中旋转中心默认值为图片中心可输入（-1， -1）", angle, rx, ry);
+			tools.DisplayEffect(img, tools.GeometricTransform.Rotation(img, angle, rx, ry));
+			break;
+		case 3:
+			InputMessage("请输入放缩倍数（等比例放缩）", multiple);
+			tools.DisplayEffect(img, tools.GeometricTransform.Scaling(img, multiple));
+			break;
+		}
+
+		InputMessage("是否继续处理（0：否，1：是）", state);
+		system("cls");
+
+		if (!state)
+			break;
+	}
 }
 
 
@@ -84,7 +112,9 @@ int main() {
 	// test1();
 	// test2();
 	// test3();
-	test4();
+	// test4();
+	// test5();
+	test6();
 
     // TODO: 暂停，system("pause");
 }
